c51ac5f1b9e771b7da8083a310da213d3bbbb31f
Roman Chuchev <romanok20@gmail.com>
2022-08-06 21:41:40 +0300

    Add isolate transformations in class SwapiService

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -7,37 +7,27 @@ export default class RandomPlanet extends Component {
   swapiService = new SwapiService()
   
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null
+    planet: {}
   }
 
   constructor(){
     super()
     setInterval(() => this.updatePlanet(), 3000)
-    this.updatePlanet()
   }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({planet})
+  };
 
   updatePlanet(){
     const id = Math.floor(Math.random() * 25 + 2)
     this.swapiService
       .getPlanet(id)
-      .then((planet) => {
-        this.setState({
-          id, 
-          name: planet.name,
-          population: planet.population,
-          rotationPeriod: planet.rotation_period,
-          diameter: planet.diameter
-        })
-
-      })
+      .then(this.onPlanetLoaded)
   }
 
   render() {
-    const {id, name, population, rotationPeriod, diameter} = this.state
+    const {planet: {id, name, population, rotationPeriod, diameter}} = this.state
     return (
       <div className="random-planet jumbotron rounded">
         <img className="planet-image"
@@ -58,6 +48,7 @@ export default class RandomPlanet extends Component {
               <span className="term">Diameter:</span>
               <span>{diameter}</span>
             </li>
+             
           </ul>
         </div>
       </div>
