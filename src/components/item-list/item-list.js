f3edd8d0635c249e514c541d04dc73968c09cb7f
Roman Chuchev <romanok20@gmail.com>
2022-08-06 19:10:12 +0300

    Add randon planet generation

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -7,13 +7,13 @@ export default class ItemList extends Component {
   render() {
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
+        <li className="list-group-item sky">
           Luke Skywalker
         </li>
-        <li className="list-group-item">
+        <li className="list-group-item sky">
           Darth Vader
         </li>
-        <li className="list-group-item">
+        <li className="list-group-item sky">
           R2-D2
         </li>
       </ul>
