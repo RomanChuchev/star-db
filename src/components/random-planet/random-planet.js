a88f14e0aa63f61de065140e66f825f76ac1ab73
Roman Chuchev <romanok20@gmail.com>
2022-08-12 19:51:08 +0300

    Refactoring: make code cleaner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,21 +1,33 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import Spinner from "../spinner";
-import ErrorIndicator from "../error-indicator";
-import SwapiService from "../../services/swapi-service";
-import "./random-planet.css";
+import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
+import SwapiService from '../../services/swapi-service';
+
+import './random-planet.css';
 
 export default class RandomPlanet extends Component {
+
+  static defaultProps = {
+    updateInterval: 10000
+  };
+
+  static propTypes = {
+    updateInterval: PropTypes.number
+  };
+
   swapiService = new SwapiService();
 
   state = {
     planet: {},
-    loading: true,
+    loading: true
   };
 
   componentDidMount() {
+    const { updateInterval } = this.props;
     this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 10000);
+    this.interval = setInterval(this.updatePlanet, updateInterval);
   }
 
   componentWillUnmount() {
@@ -26,19 +38,19 @@ export default class RandomPlanet extends Component {
     this.setState({
       planet,
       loading: false,
-      error: false,
+      error: false
     });
   };
 
   onError = (err) => {
     this.setState({
       error: true,
-      loading: false,
+      loading: false
     });
   };
 
   updatePlanet = () => {
-    const id = Math.floor(Math.random() * 17) + 2;
+    const id = Math.floor(Math.random()*17) + 2;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
@@ -50,9 +62,9 @@ export default class RandomPlanet extends Component {
 
     const hasData = !(loading || error);
 
-    const errorMessage = error ? <ErrorIndicator /> : null;
+    const errorMessage = error ? <ErrorIndicator/> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = hasData ? <PlanetView planet={planet} /> : null;
+    const content = hasData ? <PlanetView planet={planet}/> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
@@ -62,18 +74,19 @@ export default class RandomPlanet extends Component {
       </div>
     );
   }
+
 }
 
 const PlanetView = ({ planet }) => {
-  const { id, name, population, rotationPeriod, diameter } = planet;
+
+  const { id, name, population,
+    rotationPeriod, diameter } = planet;
 
   return (
     <React.Fragment>
-      <img
-        className="planet-image"
-        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-        alt="planet"
-      />
+      <img className="planet-image"
+           src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+           alt="planet" />
       <div>
         <h4>{name}</h4>
         <ul className="list-group list-group-flush">
