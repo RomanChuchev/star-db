8b6e92be132dcb378614a96392c0f92d90f6dde5
Roman Chuchev <romanok20@gmail.com>
2022-08-12 16:08:14 +0300

    Add passing component props using props.children

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -2,55 +2,48 @@ import React, { Component } from "react";
 
 import ItemList from "../item-list/item-list";
 import PersonDetails from "../person-details/person-details";
-import ErrorIndicator from "../error-indicator/error-indicator";
-
 import "./people-page.css";
 import SwapiService from "../../services/swapi-service";
 import Row from "../row/row";
+import ErrorBoundry from "../error-boundry";
 
 export default class PeoplePage extends Component {
   swapiService = new SwapiService();
 
   state = {
     selectedPerson: 3,
-    hasError: false,
   };
-
-  componentDidCatch() {
-    this.setState({
-      hasError: true,
-    });
-  }
 
   onPersonSelected = (selectedPerson) => {
     this.setState({ selectedPerson });
   };
 
   render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator />;
-    }
-
     const itemList = (
       <ItemList
         onItemSelected={this.onPersonSelected}
         getData={this.swapiService.getAllPeople}
-        renderItem={({ name, gender, birthYear }) => (
+      >
+        {(i) => (
           <div className="row mb-3">
             <div className="position-relative">
-              <div className="position-absolute top-0 start-0">{name}</div>{" "}
-              <div className="position-absolute top-0 start-50">{gender}</div>
-              <div className="position-absolute top-0 end-0">{birthYear}</div>
+              <div className="position-absolute top-0 start-0">{i.name}</div>{" "}
+              <div className="position-absolute top-0 start-50">{i.gender}</div>
+              <div className="position-absolute top-0 end-0">{i.birthYear}</div>
             </div>
           </div>
         )}
-      />
+      </ItemList>
     );
 
     const personDetales = (
       <PersonDetails personId={this.state.selectedPerson} />
     );
 
-    return <Row left={itemList} right={personDetales} />;
+    return (
+      <ErrorBoundry>
+        <Row left={itemList} right={personDetales} />;
+      </ErrorBoundry>
+    );
   }
 }
